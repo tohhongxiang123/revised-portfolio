b1a663c1b1fa58c6c150839a22862156846ac7ce
Toh Hong Xiang <tohhongxiang@gmail.com>
2023-12-15 02:56:02 +0800

    Add breadcrumb

---
+++ src/app/notes/[...id]/_components/Breadcrumbs.tsx
@@ -0,0 +1,24 @@
+import { IconChevronRight } from "@tabler/icons-react";
+
+interface BreadCrumbsProps {
+    parts: string[];
+}
+
+export default function BreadCrumbs({ parts }: BreadCrumbsProps) {
+    return (
+        <ul className="flex py-2" aria-label="breadcrumb">
+            {parts.map((part, index) => (
+                <li key={index} className="flex items-center">
+                    {index > 0 && (
+                        <IconChevronRight size={16} className="mx-2" />
+                    )}
+                    <span
+                        className="inline-flex items-center text-sm font-medium text-slate-700 dark:text-slate-400"
+                    >
+                        {decodeURI(part)}
+                    </span>
+                </li>
+            ))}
+        </ul>
+    );
+}
--- src/app/notes/[...id]/page.tsx
+++ src/app/notes/[...id]/page.tsx
@@ -2,6 +2,7 @@ import getNote from "@/utils/get-note";
 
 import { use } from "react";
 import Post from "./_post";
+import BreadCrumbs from "./_components/Breadcrumbs";
 
 interface SpecificNotePageProps {
     params: {
@@ -15,6 +16,8 @@ export default function SpecificNotePage({
     const content = use(getNote(["notes"].concat(params.id)))
     return (
         <div>
+            <BreadCrumbs parts={params.id} />
+            <div className="my-4" />
             <Post rawStringContent={content} />
         </div>
     );

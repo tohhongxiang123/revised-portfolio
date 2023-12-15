6d55ec3c78125b164fa65ceebd1c4fdc3eb60bf8
Toh Hong Xiang <tohhongxiang@gmail.com>
2023-12-16 01:28:04 +0800

    Add styling for nested sidebar

--- src/utils/get-notes.ts
+++ src/utils/get-notes.ts
@@ -1,28 +1,34 @@
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import path from "path";
 
 export type NestedDirectoryStructure = {
-    name: string,
-    fullPath: string,
-    children: NestedDirectoryStructure[]
-}
+    name: string;
+    fullPath: string;
+    children: NestedDirectoryStructure[];
+};
 
 export default function getNotes(folder: string): NestedDirectoryStructure[] {
-    const filesOrFolders = fs.readdirSync(folder)
+    const collator = new Intl.Collator(undefined, {
+        numeric: true,
+        sensitivity: "base",
+    });
+    const filesOrFolders = fs.readdirSync(folder).sort(collator.compare);
 
-    const nested = filesOrFolders.map(fileOrFolder => {
-        const name = fileOrFolder.replace(".md", "")
-        const fullPath = path.join(folder, fileOrFolder)
-        let children: NestedDirectoryStructure[] = []
+    const nested = filesOrFolders.map((fileOrFolder) => {
+        const name = fileOrFolder.replace(".md", "");
+        const fullPath = path.join(folder, fileOrFolder);
+        let children: NestedDirectoryStructure[] = [];
 
         if (fs.lstatSync(fullPath).isDirectory()) {
-            children = getNotes(fullPath)
+            children = getNotes(fullPath);
         }
 
         return {
-            name, fullPath, children
-        }
-    })
+            name,
+            fullPath,
+            children,
+        };
+    });
 
-    return nested
+    return nested;
 }

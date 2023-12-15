ca8e51aea8f82b4faec1ba598d7b22155bca5652
Toh Hong Xiang <tohhongxiang@gmail.com>
2023-12-16 03:25:27 +0800

    Expand sidebar to show current active item

--- src/components/NestedSidebar.tsx
+++ src/components/NestedSidebar.tsx
@@ -5,6 +5,23 @@ import { IconChevronDown, IconChevronRight } from "@tabler/icons-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+const nestedNavVariants = {
+    hidden: { height: 0, opacity: 0, transition: { duration: 0.1 } },
+    show: {
+        height: "auto",
+        opacity: 1,
+        transition: { duration: 0.1, delayChildren: 0.1 },
+    },
+    exit: { height: 0, opacity: 0, transition: { duration: 0.1 } },
+};
+
+const nestedNavChildrenVariants = {
+    hidden: { opacity: 0, transition: { duration: 0.1 } },
+    show: { opacity: 1, transition: { duration: 0.1 } },
+    exit: { opacity: 0, transition: { duration: 0.1 } },
+};
 
 export default function NestedLayout({
     items,
@@ -17,22 +34,26 @@ export default function NestedLayout({
 }
 
 function NestedDirectoryItem({ item }: { item: NestedDirectoryStructure }) {
-    const [isOpen, setIsOpen] = useState(false);
+    const path = usePathname();
+    const decodedPath = decodeURIComponent(path);
+
+    const [isOpen, setIsOpen] = useState(checkIfOpen(item, decodedPath));
     const handleOpen = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.stopPropagation();
         setIsOpen((c) => !c);
     };
 
-    const path = usePathname();
-    const isItemActive =
-        decodeURI(path) === "/" + item.fullPath.replaceAll("\\", "/");
+    const isItemActive = arrayEquals(
+        item.fullPath.split(sep).filter(Boolean),
+        decodeURIComponent(path).split(sep).filter(Boolean)
+    );
 
     if (item.children.length === 0) {
         return (
             <Link
                 href={`/${item.fullPath}`}
-                className={`relative flex w-full cursor-pointer items-center justify-between rounded-md p-2 text-left text-sm hover:bg-gray-500/30 ${
-                    isItemActive ? "font-bold" : ""
+                className={`relative flex w-full cursor-pointer items-center justify-between rounded-md p-2 text-left text-sm hover:bg-gray-500/10 ${
+                    isItemActive ? "bg-gray-500/10 font-bold" : ""
                 }`}
             >
                 {item.name}
@@ -55,15 +76,62 @@ function NestedDirectoryItem({ item }: { item: NestedDirectoryStructure }) {
                     <IconChevronRight size={16} className="mx-2" />
                 )}
             </button>
-            {isOpen && (
-                <ul className="ml-4 border-l border-gray-700/30">
-                    {item.children.map((child) => (
-                        <li key={child.fullPath} className="pl-1.5">
-                            <NestedDirectoryItem item={child} />
-                        </li>
-                    ))}
-                </ul>
-            )}
+            <AnimatePresence>
+                {isOpen && (
+                    <motion.ul
+                        variants={nestedNavVariants}
+                        initial="hidden"
+                        animate="show"
+                        exit="exit"
+                        className="ml-4 border-l border-gray-700/30"
+                    >
+                        {item.children.map((child) => (
+                            <motion.li
+                                variants={nestedNavChildrenVariants}
+                                initial="hidden"
+                                animate="show"
+                                exit="exit"
+                                key={child.fullPath}
+                                className="pl-1.5"
+                            >
+                                <NestedDirectoryItem item={child} />
+                            </motion.li>
+                        ))}
+                    </motion.ul>
+                )}
+            </AnimatePresence>
         </>
+    );
+}
+
+const sep = /\\|\//g; // possible separators
+
+function arrayEquals<T>(arr1: T[], arr2: T[]) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function checkIfOpen(
+    item: NestedDirectoryStructure,
+    currentPath: string
+): boolean {
+    if (item.children.length == 0) {
+        return arrayEquals(
+            item.fullPath.split(sep).filter(Boolean),
+            currentPath.split("/").filter(Boolean)
+        );
+    }
+
+    return item.children.some((child) =>
+        checkIfOpen({ ...child }, currentPath)
     );
 }
